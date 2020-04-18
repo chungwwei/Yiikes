da6783916bd72e44117e4b8e59ef17d0e66ce5f4
Henry Li <xiaofengli2016@gmail.com>
2020-04-18 18:53:54 -0400

    level select minor fixes

--- src/level_select.js
+++ src/level_select.js
@@ -25,24 +25,33 @@ export class LevelSelectScene extends Phaser.Scene {
         this.btReturn.on('pointerdown', () => {
             this.scene.start('main_screen')
         })
+
+        this.controlText = this.add.text(310, 100, 'Level Select')
+        this.controlText.setColor('0x000000')
+        this.controlText.setFontSize(50)
         
         this.texts = []
 
         for (let i = 0; i < 9; i ++) {
             var x, y;
-            if (i < 3) y = 200
-            else if (i < 6) y = 300
-            else if (i < 9) y = 400
+            if (i < 3) y = 250
+            else if (i < 6) y = 400
+            else if (i < 9) y = 550
 
             if (i % 3 === 0) x = 250
-            else if (i % 3 === 1) x = 350
-            else if (i % 3 === 2) x = 450
+            else if (i % 3 === 1) x = 500
+            else if (i % 3 === 2) x = 750
 
             var img
             if (gameState.levelCompletion[i] === false)
                 img = this.add.image(x, y, 'lock')
-            else
+            else{
                 img = this.add.image(x, y, 'unlock')
+                img.setInteractive()
+                img.on('pointerdown', () =>{
+                    this.scene.start('level' + (i + 1))
+                })
+            }
             let text = this.add.text(img.x, 40 + img.y, "level " + (i + 1))
             text.setOrigin(0.5)
             text.setColor('0x000000')
@@ -54,9 +63,11 @@ export class LevelSelectScene extends Phaser.Scene {
         for (let i = 0; i < this.texts.length; i ++) {
             var text = this.texts[i];
             console.log(text)
-            text.on('pointerdown', () => {
-                this.scene.start('level' + (i + 1))
-            })
+            if(gameState.levelCompletion[i] === true){
+                text.on('pointerdown', () => {
+                    this.scene.start('level' + (i + 1))
+                })
+            }
         }
     }
 
